0fb0be229e8248916e3ec96bcdacefd3347f9750
Lance <mhoneylance@gmail.com>
2023-09-23 01:52:16 +0800

    Ch. 18 - User Roles and Permissions

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-require get_theme_file_path( '/inc/search-route.php' );
+require get_theme_file_path('/inc/search-route.php');
 
 function university_custom_rest()
 {
@@ -104,4 +104,53 @@ function universityMapKey($api)
 }
 
 add_filter('acf/fields/google_map/api', 'universityMapKey');
+
+// Redirect subscriber accounts out of admin and onto homepage
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function redirectSubsToFrontend()
+{
+    $ourCurrentUser = wp_get_current_user();
+
+    if (1 == count($ourCurrentUser->roles) and 'subscriber' == $ourCurrentUser->roles[0]) {
+        wp_redirect(site_url('/'));
+
+        exit;
+    }
+}
+add_action('wp_loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar()
+{
+    $ourCurrentUser = wp_get_current_user();
+
+    if (1 == count($ourCurrentUser->roles) and 'subscriber' == $ourCurrentUser->roles[0]) {
+        show_admin_bar(false);
+    }
+}
+
+// Customize Login Screen
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl()
+{
+    return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS()
+{
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle()
+{
+    return get_bloginfo('name');
+}
 ?>
